208cda75fa5f355adc0e9112c10f6888aa4d05f5
Reyfadh07 <raihan.farmasi@gmail.com>
2023-03-31 22:08:00 +0700

    Menambahkan Password dan text untuk mengimput password serta menambahkan ucon kunci dan red eye ,menambahkan elevated button dan mengganti warnanya

--- lib/loginpage.dart
+++ lib/loginpage.dart
@@ -42,37 +42,76 @@ class _LoginPageState extends State<LoginPage> {
             ),
           ),
           Form(
-              child: Column(
-            children: [
-              Container(
-                padding: const EdgeInsets.only(top: 30, left: 30),
-                alignment: Alignment.topLeft,
-                child: const Text(
-                  "Nama",
-                  textAlign: TextAlign.left,
-                  style: TextStyle(
-                      color: Colors.redAccent, fontWeight: FontWeight.bold),
+            child: Column(
+              children: [
+                Container(
+                  padding: const EdgeInsets.only(top: 30, left: 30),
+                  alignment: Alignment.topLeft,
+                  child: const Text(
+                    "Nama",
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                        color: Colors.redAccent, fontWeight: FontWeight.bold),
+                  ),
                 ),
-              ),
-              Container(
-                padding: const EdgeInsets.only(left: 20, right: 20),
-                child: TextFormField(
-                  decoration: const InputDecoration(
-                    border: UnderlineInputBorder(),
-                    hintText: 'Masukan Nama',
-                    hintStyle: TextStyle(fontSize: 20),
-                    prefixIcon: Icon(
-                      Icons.person_2,
-                      color: Colors.redAccent,
+                Container(
+                  padding: const EdgeInsets.only(left: 20, right: 20),
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                      border: UnderlineInputBorder(),
+                      hintText: 'Masukan Nama',
+                      hintStyle: TextStyle(fontSize: 20),
+                      prefixIcon: Icon(
+                        Icons.person_2,
+                        color: Colors.redAccent,
+                      ),
                     ),
                   ),
                 ),
-              ),
-              Container(
-                child: Text("Password"),
-              )
-            ],
-          ))
+                Container(
+                  padding: const EdgeInsets.only(top: 30, left: 30),
+                  alignment: Alignment.topLeft,
+                  child: const Text(
+                    "Password",
+                    textAlign: TextAlign.left,
+                    style: TextStyle(
+                        color: Colors.redAccent, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.only(left: 20, right: 20),
+                  child: TextFormField(
+                    decoration: const InputDecoration(
+                        border: UnderlineInputBorder(),
+                        hintText: 'Masukan Password',
+                        hintStyle: TextStyle(fontSize: 20),
+                        prefixIcon: Icon(
+                          Icons.lock,
+                          color: Colors.redAccent,
+                        ),
+                        suffixIcon: Icon(Icons.remove_red_eye)),
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.only(top: 100),
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50),
+                        ),
+                        backgroundColor: Colors.redAccent,
+                        minimumSize: const Size(350, 60)),
+                    child: const Text(
+                      "Masuk",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ),
+                Row()
+              ],
+            ),
+          ),
         ]),
       ),
     );
